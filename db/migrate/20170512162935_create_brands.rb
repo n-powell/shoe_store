48f0b24e941c2c88e5cf58cc8d8937d4148e319f
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-12 09:31:58 -0700

    add brands table to database

---
+++ db/migrate/20170512162935_create_brands.rb
@@ -0,0 +1,9 @@
+class CreateBrands < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:brands) do |t|
+      t.column(:name, :string)
+      t.column(:price, :integer)
+      t.timestamps
+    end
+  end
+end
