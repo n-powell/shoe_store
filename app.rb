f7999ffecf45cdfc8f5a0b0843708ad38a628fd5
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-12 10:20:34 -0700

    add store post functionality on new store page

--- app.rb
+++ app.rb
@@ -10,3 +10,14 @@ get "/stores" do
   @all_stores = Store.all
   erb(:stores)
 end
+
+get "/store/new" do
+  @all_stores = Store.all
+  erb(:new_store)
+end
+
+post "/store/new" do
+  store_name = params['store_name']
+  new_store = Store.create(name: store_name)
+  erb(:index)
+end
