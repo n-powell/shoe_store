7642d4f9fcb9c01df6e43baaef3747bfe698537f
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-12 11:50:30 -0700

    add validation of name input to store creation. add callback to capitalize input of brand.

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
+  validates(:name, :presence => true)
+  before_create(:capitalize)
+
+
+  private
+  def capitalize
+    self.name = name.capitalize
+  end
 end
