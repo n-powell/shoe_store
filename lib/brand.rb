8f37fd3ee14b794bd6cbd29b75d7d028cc175896
Nick Powell <nickpowell@Nicks-iMac.local>
2017-05-12 12:01:10 -0700

    clean up validation and callback syntax format.

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,8 +1,9 @@
 class Brand < ActiveRecord::Base
-  has_and_belongs_to_many(:stores)
-  validates(:name, :presence => true)
-  validates(:price, :presence => true)
-  before_create(:capitalize)
+  has_and_belongs_to_many :stores
+  validates :name, presence: true
+  validates :price, presence: true
+  before_create :capitalize
+  validates :name, uniqueness: true
 
 
   private
--- lib/store.rb
+++ lib/store.rb
@@ -1,7 +1,7 @@
 class Store < ActiveRecord::Base
-  has_and_belongs_to_many(:brands)
-  validates(:name, :presence => true)
-  before_create(:capitalize)
+  has_and_belongs_to_many :brands
+  validates :name, presence: true
+  before_create :capitalize
   validates :name, uniqueness: true
 
 
